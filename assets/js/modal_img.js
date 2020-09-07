3e718fd6670f70f6776165545558586fb8f8e08f
tchying <66045758+tchying@users.noreply.github.com>
2020-09-06 22:36:42 -0400

    Update modal_img.js

--- assets/js/modal_img.js
+++ assets/js/modal_img.js
@@ -6,17 +6,24 @@ var modal = document.getElementsByClassName('modal');
 // Get the button that opens the modal
 var btn = document.getElementsByClassName("myBtn");
 
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close");
+
+var img = document.getElementsByClassName("myImg");
+var modalImg = document.getElementsByClassName("img01");
+var captionText = document.getElementsByClassName("caption");
 
 // When the user clicks the button, open the modal 
 btn[0].onclick = function() {
     modal[0].style.display = "block";
+    modalImg[0].src = this.src;
+    captionText[0].innerHTML = this.alt;
 }
 
 btn[1].onclick = function() {
     modal[1].style.display = "block";
+    modalImg[1].src = this.src;
+    captionText[1].innerHTML = this.alt;
 }
 // When the user clicks on <span> (x), close the modal
 span[0].onclick = function() {
