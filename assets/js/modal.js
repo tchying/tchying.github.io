4b346981b5860ef939ba0fb08a31a7f2d062544b
tchying <66045758+tchying@users.noreply.github.com>
2020-09-06 22:24:56 -0400

    Update modal.js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,5 +1,6 @@
 /*
 https://www.w3schools.com/howto/howto_css_modal_images.asp
+https://stackoverflow.com/questions/37402241/support-for-multiple-modal-single-page
 */
 
 // Get the modal
@@ -9,16 +10,32 @@ var modal = document.getElementById("myModal");
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+
+img[0].onclick = function(){
+  modal[0].style.display = "block";
+  modalImg[0].src = this.src;
+  captionText[0].innerHTML = this.alt;
+}
+img[1].onclick = function(){
+  modal[1].style.display = "block";
+  modalImg[1].src = this.src;
+  captionText[1].innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
+span[0].onclick = function() { 
+  modal[0].style.display = "none";
+}
+span[1].onclick = function() { 
+  modal[1].style.display = "none";
+}
+
+// Click outside
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
